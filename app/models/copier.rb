a0507b59d69578c95df2d009920469b79cf7a73e
michalubczyk <lubczyk.michal@gmail.com>
2015-09-03 11:11:12 +0200

    admin panel #2

--- app/models/copier.rb
+++ app/models/copier.rb
@@ -2,4 +2,7 @@ class Copier < ActiveRecord::Base
   attr_accessible :nazwa, :stan, :cena, :promocja, :funkcjonalnosc_st, :funkcjonalnosc_op, :predkosc_drukowania, :duplex,
                   :formaty_oryginalow, :podajnik_papieru_st, :gramatura_papieru, :rozdzielczosc_drukowania, :rozdzielczosc_kopiowania,
                   :tryb_skanowania, :drukowanie_sieciowe, :podajnik_oryginalow_st, :dysk_hdd, :druk_nosniki_danych, :czas_nagrzewania
+
+  validates :nazwa, presence: { message: "Wprowadź nazwę kserokopiarki." }
+  validates :cena, presence: { message: "Wprowadź cenę kserokopiarki." }
 end
